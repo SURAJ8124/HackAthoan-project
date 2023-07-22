4e373bb20634c8c5d83a085f16ad4ee3308d1fd1
SURAJ8124 <surajchavan1532@gmail.com>
2023-07-22 10:59:08 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavigationBar from './Components/NavigationBar';
-import Home from './Components/Home';
-import About from './Components/About';
-import Services from './Components/Services';
-import Contact from './Components/Contact';
-import './App.css';
 
 const App = () => {
   return (
     <>
-    <Router>
-    <div>
-      {/* <NavigationBar /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
-  </Router>
+    <div>Hello Word</div>
   </>
   );
 };
